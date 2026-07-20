d5ac0f99ea6ae692f29754078fb5b24d7a49936c
Satyamtiwari23 <sttiwari9211@gmail.com>
2026-07-20 16:14:23 +0530

    Organized and add a project

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
+app.get("/", (req, res) => {
+  res.json({
+    status: "Online",
+    message: "Portfolio Backend is Running 🚀",
+    endpoints: {
+      reviews: "/api/reviews"
+    }
+  });
+});
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
